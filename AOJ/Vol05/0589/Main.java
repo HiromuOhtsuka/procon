ac184a86ab9fec50077e6fac45b56d0f33b395bf
HiromuOhtsuka <multiverse.yume.a12@gmail.com>
2016-05-09 01:06:19 +0900

    修正し，通るようにした

--- AOJ/Vol05/0589/Main.java
+++ AOJ/Vol05/0589/Main.java
@@ -1,47 +1,41 @@
 import java.util.Scanner;
 import java.util.Map;
-import java.util.HashMap;
-import java.util.Set;
-import java.util.Arrays;
+import java.util.TreeMap;
 import java.util.Comparator;
 
 public class Main {
   public static void main(String[] args){
-    Scanner scan = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
 
-    int N = scan.nextInt();
-    Map< String, Integer > map = new HashMap< String, Integer >();
+    int N = sc.nextInt();
+    Map< String, Integer > map = 
+      new TreeMap< String, Integer >(
+          new Comparator< String >(){
+          @Override
+          public int compare(String s1, String s2){
+          if(s1.length() == s2.length()){
+          return s1.compareTo(s2);
+          }
+          return s1.length() - s2.length();
+          };
+          }
+          );
+
     for(int i = 0; i < N; i++){
-      String name = scan.next();
-      int count = scan.nextInt();
-      Integer value = map.get(name);
-      if(value == null){
-        map.put(name, count);
+      String name = sc.next();
+      int count = sc.nextInt();
+      if(map.containsKey(name)){
+        int v = map.get(name);
+        map.remove(name);
+        map.put(name, v + count);
       }
       else{
-        map.remove(name);
-        map.put(name, value + count);
+        map.put(name, count);
       }
     }
-    Map.Entry[] a = 
-      map.entrySet().toArray(new Map.Entry[map.size()] );
-    Arrays.sort(a, new Comparator(){
-        @Override
-        public int compare(Object o1, Object o2){
-          Map.Entry< String, Integer > e1 = (Map.Entry< String, Integer >) o1,
-          e2 = (Map.Entry< String, Integer >) o2;
-        String s1 = e1.getKey(), s2 = e2.getKey();
-        if(s1.length() == s2.length()){
-        return s1.compareTo(s2);
-        }
-        return s1.length() - s2.length();
-        }
-        }
-        );
 
-    for(int i = 0; i < a.length; i++){
-      System.out.println(a[i].getKey() + " " + a[i].getValue());
+    for(Map.Entry< String, Integer > me : map.entrySet()){
+      System.out.println(me.getKey() + " " +  me.getValue());
     }
-
   }
 }
