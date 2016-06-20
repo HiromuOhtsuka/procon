28bd0f6b399652b4a9f8955489271cff970d3a3c
HiromuOhtsuka <multiverse.yume.a12@gmail.com>
2016-06-21 01:18:37 +0900

    pending

--- Codeforces/Div2#357/D/Main.java
+++ Codeforces/Div2#357/D/Main.java
@@ -6,108 +6,81 @@ import java.util.ArrayList;
 
 public class Main {
   static int n, m;
-  static Set< Edge > edges;
-  static Set< Integer > vertices;
-  static Set< Integer > ll;
   static int[] l;
-  static int[] rev;
+  static int[] rev, to;
 
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
 
     n = sc.nextInt(); m = sc.nextInt();
-    edges = new TreeSet< Edge >();
-    rev = new int[n];
+    to = new int[n]; rev = new int[n];
     for(int i = 0; i < n; i++){
-      rev[i] = -1;
+      rev[i] = to[i] = -1;
     }
     for(int i = 0; i < m; i++){
       int v = sc.nextInt() - 1, w = sc.nextInt() - 1;
-      edges.add(new Edge(v, w));
+      to[v] = w;
       rev[w] = v;
     }
     l = new int[n];
     for(int i = 0; i < n; i++){
-      l[i] = sc.nextInt();
+      l[i] = sc.nextInt() - 1;
     }
 
-    vertices = new TreeSet< Integer >();
-    for(int v = 0; v < n; v++){
-      vertices.add(v);
-    }
-    ll = new TreeSet< Integer >();
-
-    List< Set< Integer > > parents = new ArrayList< Set< Integer > >();
+    List< Set< Integer > > childs = new ArrayList< Set< Integer > >();
     for(int i = 0; i < n; i++){
-      parents.add(new TreeSet< Integer >());
+      childs.add(new TreeSet< Integer >());
     }
 
     for(int i = 0; i < n; i++){
-      Set< Integer > s = parents.get(i);
-      int v = rev[i];
+      Set< Integer > s = childs.get(i);
+      int v = to[i];
       while(v != -1){
         s.add(v);
-        v = rev[v];
+        v = to[v];
+      }
+    }
+
+    Set< Integer > leafs = new TreeSet< Integer >();
+    for(int i = 0; i < n; i++){
+      if(to[i] == -1){
+        leafs.add(i);
       }
     }
 
     int[] list = new int[n];
     int sp = 0;
-    for(;;){
-      int[] outd = new int[n];
-      for(Edge e : edges){
-        ++outd[e.v];
-      }
-      boolean flag = false;
-      for(int i = 0; i < n; i++){
-        if(vertices.contains(i) && outd[i] == 0 && !ll.contains(l[i])){
-          Set< Integer > parent = parents.get(l[i] - 1);
-          for(int j = 0; j < sp; j++){
-            if(parent.contains(list[j] - 1)){
-              System.out.println("-1");
-              return;
-            }
+    boolean[] check = new boolean[n];
+    for(Integer leaf : leafs){
+      Set< Integer > child = null;
+      int lf = leaf;
+      while(lf != -1){
+        if(check[l[lf]]){
+          Set< Integer > tmp = childs.get(l[lf]);
+          if(!tmp.contains(lf)){
+            lf = rev[lf];
           }
-          ll.add(l[i]);
-          list[sp++] = l[i];
-          edges.remove(new Edge(rev[i], i));
-          vertices.remove(i);
-          flag = true;
+          else{
+            System.out.println(-1);
+            return;
+          }
         }
-      }
-      if(!flag){
-        break;
+        else if(child == null || (!child.contains(l[lf]))){
+          list[sp++] = l[lf];
+          check[l[lf]] = true;
+          child = childs.get(l[lf]);
+          lf = rev[lf];
+        }
+        else{
+          System.out.println(-1);
+          return;
+        }
       }
     }
 
     System.out.println(sp);
     for(int i = 0; i < sp; i++){
-      System.out.println(list[i]);
+      System.out.println(list[i] + 1);
     }
-  }
-
-  static class Edge implements Comparable< Edge > {
-    int v, w;
-
-    Edge(int v, int w){
-      this.v = v; this.w = w;
-    }
-
-    @Override
-      public boolean equals(Object obj){
-        if(!(obj instanceof Edge)){
-          return false;
-        }
-        Edge e = (Edge)obj;
-        return v == e.v && w == e.w;
-      }
-
-    @Override
-      public int compareTo(Edge e){
-        if(v == e.v){
-          return w - e.w;
-        }
-        return v - e.v;
-      }
   }
 }
