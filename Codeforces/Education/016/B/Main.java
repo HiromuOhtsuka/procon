3de904d6ded3e37602c81fdc39dae3d3cbd5cf21
HiromuOhtsuka <multiverse.yume.a12@gmail.com>
2016-08-23 13:35:39 +0900

    fix Educational#016 B

--- Codeforces/Education/016/B/Main.java
+++ Codeforces/Education/016/B/Main.java
@@ -3,39 +3,24 @@ import java.util.Arrays;
 
 public class Main {
   static int n;
-  static int[] a;
+  static Integer[] a;
 
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
 
     n = sc.nextInt();
-    a = new int[n];
+    a = new Integer[n];
     for(int i = 0; i < n; i++){
       a[i] = sc.nextInt();
     }
 
     Arrays.sort(a);
 
-    int left = a[0], right = a[n - 1];
-    for(int i = 0; i < 100; i++){
-      if(f((left * 2 + right) / 3) <= f((left + right * 2) / 3)){
-        right = (left + right * 2) / 3;
-      }
-      else{
-        left = (left * 2 + right) / 3;
-      }
+    if(n % 2 == 0){
+      System.out.println(a[n / 2 - 1]);
     }
-
-    int ans = (left + right) / 2;
-
-    System.out.println(ans);
-  }
-
-  private static long f(int x){
-    long sum = 0;
-    for(int i = 0; i < n; i++){
-      sum += Math.abs(a[i] - x);
+    else{
+      System.out.println(a[n / 2]);
     }
-    return sum;
   }
 }
