a5ad62c1d075c5d4280838c1ad0cc834075c2d7a
HiromuOhtsuka <multiverse.yume.a12@gmail.com>
2016-08-30 11:01:55 +0900

    Div2#369 C

--- Codeforces/Div2/369/C/Main.java
+++ Codeforces/Div2/369/C/Main.java
@@ -12,12 +12,8 @@ public class Main {
 
     n = sc.nextInt(); m = sc.nextInt(); k = sc.nextInt();
     c = new int[n];
-    boolean check = true;
     for(int i = 0; i < n; i++){
       c[i] = sc.nextInt();
-      if(c[i] == 0){
-        check = false;
-      }
     }
     p = new int[n][m];
     for(int i = 0; i < n; i++){
@@ -25,57 +21,49 @@ public class Main {
         p[i][j] = sc.nextInt();
       }
     }
-    if(check){
-      if(beauty(c, n) == k){
-        System.out.println(0);
-      }
-      else{
-        System.out.println(-1);
-      }
-      return;
-    }
 
     long[][][] dp = new long[n + 1][k + 1][m + 1];
 
-    for(int i = 0; i <= n; i++){
-      for(int j = 0; j <= k; j++){
-        Arrays.fill(dp[i][j], INF);
+    for(int i = 0; i < dp.length; i++){
+      for(int j = 0; j < dp[i].length; j++){
+          Arrays.fill(dp[i][j], INF);
       }
     }
-    if(c[0] != 0){
+    if(c[0] == 0){
       for(int i = 1; i <= m; i++){
-        dp[1][1][i] = 0;
+        dp[1][1][i] = p[0][i - 1];
       }
     }
     else{
-      for(int i = 0; i < m; i++){
-        dp[1][1][i + 1] = p[0][i];
-      }
+      dp[1][1][c[0]] = 0;
     }
 
     for(int i = 2; i <= n; i++){
       for(int j = 1; j <= k; j++){
-        if(c[i - 1] != 0){
-          long val = dp[i - 1][j][c[i - 1]];
-          for(int l = 1; l <= m; l++){
-            if(c[i - 1] == l){
-              continue;
-            }
-            val = Math.min(val, dp[i - 1][j - 1][l]);
-          }
-          dp[i][j][c[i - 1]] = val;
-        }
-        else{
+        if(c[i - 1] == 0){
           for(int l = 1; l <= m; l++){
             long val = dp[i - 1][j][l] + p[i - 1][l - 1];
             for(int o = 1; o <= m; o++){
-              if(o == l){
+              if(l == o){
                 continue;
               }
               val = Math.min(val, dp[i - 1][j - 1][o] + p[i - 1][l - 1]);
             }
             dp[i][j][l] = val;
           }
+        }
+        else{
+          for(int l = 0; l <= m; l++){
+            dp[i][j][l] = INF;
+          }
+          long val = dp[i - 1][j][c[i - 1]];
+          for(int o = 1; o <= m; o++){
+            if(c[i - 1] == o){
+              continue;
+            }
+            val = Math.min(val, dp[i - 1][j - 1][o]);
+          }
+          dp[i][j][c[i - 1]] = val;
         }
       }
     }
@@ -91,22 +79,5 @@ public class Main {
     else{
       System.out.println(-1);
     }
-  }
-
-  private static long beauty(int[] a, int n){
-    long count = 0;
-    int s = 0, t = 0;
-    for(;;){
-      t = s;
-      while(t + 1 < n && a[t] == a[t + 1]){
-        ++t;
-      }
-      ++count;
-      s = t + 1;
-      if(s >= n){
-        break;
-      }
-    }
-    return count;
   }
 }
