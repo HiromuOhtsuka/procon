a80c540aa1ca9c346527ef112aabe97ed5cbd59c
HiromuOhtsuka <multiverse.yume.a12@gmail.com>
2018-01-11 20:03:45 +0900

    ABC#081

---
+++ AtCoder/ABC/081/A/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <climits>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <string>
+#define INF_INT (INT_MAX / 2)
+#define INF_LONG (LONG_MAX / 2)
+//#define DEBUG true
+#define DEBUG false
+using namespace std;
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  string s;
+  cin >> s;
+
+  int c = 0;
+  for(char ch: s){
+    if(ch == '1') ++c;
+  }
+
+  cout << c << endl;
+
+  return 0;
+}
