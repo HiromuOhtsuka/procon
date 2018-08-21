178c114d4d4a831208b5363e77021a71b3e3b710
HiromuOhtsuka <h.ohtsuka.yume@gmail.com>
2018-08-21 14:03:37 +0900

    ABC#106

---
+++ AtCoder/ABC/106/B/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <climits>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <string>
+#include <deque>
+#define INF_INT (INT_MAX / 2)
+#define INF_LONG (LONG_MAX / 2)
+//#define DEBUG true
+#define DEBUG false
+using namespace std;
+
+typedef long long ll;
+typedef pair< int, int > pii;
+typedef pair< ll, ll > pll;
+typedef pair< ll, int > pli;
+typedef pair< int, ll > pil;
+
+int ceil(int x, int y){
+  return (x % y == 0) ? x / y : x / y + 1;
+}
+
+int gcd(int x, int y){
+  return y ? gcd(y, x % y) : x;
+}
+
+int lcm(int x, int y){
+  return x / gcd(x, y) * y;
+}
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int n;
+  cin >> n;
+
+  int count = 0;
+  for(int i = 1; i <= n; i += 2){
+    int c = 0;
+    for(int j = 1; j <= i; j++){
+      if(i % j == 0) ++c;
+    }
+    if(c == 8) ++count;
+  }
+
+  cout << count << endl;
+
+  return 0;
+}
---
+++ AtCoder/ABC/106/C/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <climits>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <string>
+#include <deque>
+#define INF_INT (INT_MAX / 2)
+#define INF_LONG (LONG_MAX / 2)
+//#define DEBUG true
+#define DEBUG false
+using namespace std;
+
+typedef long long ll;
+typedef pair< int, int > pii;
+typedef pair< ll, ll > pll;
+typedef pair< ll, int > pli;
+typedef pair< int, ll > pil;
+
+int ceil(int x, int y){
+  return (x % y == 0) ? x / y : x / y + 1;
+}
+
+int gcd(int x, int y){
+  return y ? gcd(y, x % y) : x;
+}
+
+int lcm(int x, int y){
+  return x / gcd(x, y) * y;
+}
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  string s;
+  cin >> s;
+  ll k;
+  cin >> k;
+
+  int n = s.length();
+
+  int i = 0;
+  while(i < n && i < k - 1 && s[i] == '1'){
+    ++i;
+  }
+
+  if(i == n) cout << 1 << endl;
+  else cout << s[i] << endl;
+
+  return 0;
+}
