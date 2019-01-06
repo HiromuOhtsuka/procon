6084866f634481b7943f361e67ec23a520f6d593
HiromuOhtsuka <h.ohtsuka.yume@gmail.com>
2019-01-06 11:37:22 +0900

    CaDDi

---
+++ AtCoder/CADDi/2018/B/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <climits>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <string>
+#include <deque>
+#define INF_INT (INT_MAX / 2)
+#define INF_LONG (LONG_MAX / 2)
+//#define DEBUG true
+#define DEBUG false
+using namespace std;
+
+const int MAX = 100001;
+const int MOD = 1000000007;
+
+typedef long long ll;
+typedef pair< int, int > pii;
+typedef pair< ll, ll > pll;
+typedef pair< ll, int > pli;
+typedef pair< int, ll > pil;
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int n, h, w;
+  cin >> n >> h >> w;
+  int a[n], b[n];
+  for(int i = 0; i < n; i++) cin >> a[i] >> b[i];
+
+  int count = 0;
+  for(int i = 0; i < n; i++){
+    if(a[i] >= h && b[i] >= w) ++count;
+  }
+
+  cout << count << endl;
+
+  return 0;
+}
